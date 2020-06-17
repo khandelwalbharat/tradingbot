b3738bb5e6e13c662b97faf6d22636917971a412
Bharat Khandelwal <bharatseema@gmail.com>
2020-06-17 11:19:39 +0530

    small logging fix

--- order_manager.py
+++ order_manager.py
@@ -8,4 +8,4 @@ class OrderManager(object):
         self.symbol=symbol
     def addTrade(self, time, risk, price, trade_type, stoploss=None):
         with open("trades.txt", "a+") as f:
-            f.write("Added trade for {} at time={} with position={}, price={}, trade_type={}, stoploss={}".format(self.symbol, time, risk, price, trade_type, stoploss))
+            f.write("Added trade for {} at time={} with position={}, price={}, trade_type={}, stoploss={}\n".format(self.symbol, time, risk, price, trade_type, stoploss))
