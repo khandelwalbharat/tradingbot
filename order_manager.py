77f5a1e27889bf77e3307895b27d8db062fdd5b3
Bharat Khandelwal <bharatseema@gmail.com>
2020-06-17 10:37:10 +0530

    indentation fix

--- order_manager.py
+++ order_manager.py
@@ -1,11 +1,11 @@
 
 
 class OrderManager(object):
-	"""
-	Currently a dummy order manager which just prints the orders
-	"""
-	def __init__(self, symbol):
-		self.symbol=symbol
-	def addTrade(self, time, risk, price, trade_type, stoploss=None):
-		with open("trades.txt", "a+") as f:
-			f.write("Added trade for {} at time={} with position={}, price={}, trade_type={}, stoploss={}".format(self.symbol, time, risk, price, trade_type, stoploss))
+    """
+    Currently a dummy order manager which just prints the orders
+    """
+    def __init__(self, symbol):
+        self.symbol=symbol
+    def addTrade(self, time, risk, price, trade_type, stoploss=None):
+        with open("trades.txt", "a+") as f:
+            f.write("Added trade for {} at time={} with position={}, price={}, trade_type={}, stoploss={}".format(self.symbol, time, risk, price, trade_type, stoploss))
