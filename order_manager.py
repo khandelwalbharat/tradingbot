4bc0446a1469f5316ac9b193fe197a20a3e5d6c5
kushagra <kuku12320@gmail.com>
2020-06-22 15:20:38 +0530

    bug fixes in rounding off price

--- order_manager.py
+++ order_manager.py
@@ -19,7 +19,7 @@ class OrderManager(object):
             
             if(trade_type == 'day_end' or trade_type == 'new' or trade_type == 'trend_reversal'):
             # Place the normal order
-                trading_price = int(price // 0.05)*0.05 # Just an approximation for now, patch up later
+                trading_price = round(price / 0.05) * 0.05 # Just an approximation for now, patch up later
                 transaction_type = self.kite.TRANSACTION_TYPE_BUY if action=='buy' else self.kite.TRANSACTION_TYPE_SELL
 
                 try:
@@ -37,16 +37,16 @@ class OrderManager(object):
             if((trade_type == 'new') and stoploss is not None):
             # Place the stoploss order
                 
-                stoploss_trading_price = int(stoploss // 0.05)*0.05 # Just an approximation for now, patch up later
+                stoploss_trading_price = round(stoploss / 0.05) * 0.05 # Just an approximation for now, patch up later
                 stoploss_transaction_type = self.kite.TRANSACTION_TYPE_BUY if action=='sell' else self.kite.TRANSACTION_TYPE_SELL #Since it is stoploss, do opposite
                 offset = 0.0002*stoploss
                 if(action == 'buy'): #the stoploss is selling
                     stoploss_trigger_price = stoploss+offset
-                    stoploss_trigger_price = int(stoploss_trigger_price // 0.05)*0.05
+                    stoploss_trigger_price = round(stoploss_trigger_price / 0.05) * 0.05
                     stoploss_trigger_price = max(stoploss_trigger_price, stoploss_trading_price+0.05)
                 elif(action == 'sell'): #stoploss is buying
                     stoploss_trigger_price = stoploss-offset
-                    stoploss_trigger_price = int(stoploss_trigger_price // 0.05)*0.05
+                    stoploss_trigger_price = round(stoploss_trigger_price / 0.05) * 0.05
                     stoploss_trigger_price = min(stoploss_trigger_price, stoploss_trading_price-0.05)
 
                 try:
